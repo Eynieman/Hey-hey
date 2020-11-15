8172ac4858e82836f179ababce722838d6963058
Eynieman <eynieman@gmail.com>
2020-11-15 13:09:10 -0300

    Primer commit con carrousel

---
+++ app.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 0,
+    grabCursor: true,
+    autoplay: {
+        autoplay: true,
+        delay: 2000, disableOnInteraction: false
+    }
+});
+
