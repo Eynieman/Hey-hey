b63cb29cb60f5dc977dc30ad693a0d2f94250549
Tadeo Mocoroa <juan_tadeo25@hotmail.com>
2020-11-19 19:00:23 -0300

    login hecho

---
+++ app.js
@@ -0,0 +1,42 @@
+const formLogin = document.getElementById('formLogin');
+const userInput = document.getElementById('user')
+const passwordInput = document.getElementById('password')
+const alert = document.getElementById('alert')
+
+formLogin.onsubmit = function (e) {
+    e.preventDefault()
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const user = userInput.value;
+    const password = passwordInput.value;
+    const findOutUser = users.find((u) => u.user === user && u.password === password);
+
+
+    if (findOutUser) {
+        alert('Bienvenido/a');
+        window.location.href = './login.html'
+    } else {
+        alert.classList.remove('d-none');
+    }
+}
+
+
+const formCreate = document.getElementById('formCreate')
+const userCreateInput = document.getElementById('userCreate')
+const passwordCreateInput = document.getElementById('passwordCreate')
+
+formCreate.onsubmit = (e) => {
+    e.preventDefault()
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const user = userCreateInput.value;
+    const password = passwordCreateInput.value;
+
+
+    users.push({
+        user,
+        password,
+    })
+
+    localStorage.setItem('users', JSON.stringify(users));
+    formCreate.reset();
+    $('#modalCreateAccount').modal('hide');
+}
