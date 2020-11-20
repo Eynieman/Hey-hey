3c3a942c7b118ac22e49b830b0741227bb11e924
Eynieman <eynieman@gmail.com>
2020-11-20 17:43:36 -0300

    Creado el login de admin1

---
+++ loginAdm.js
@@ -0,0 +1,25 @@
+/*--------------------------LOGIN ADMIN-------------------------*/
+const formLoginAdm = document.getElementById('formLogin');
+const inputUserAdm = document.getElementById('user');
+const inputPasswordAdm = document.getElementById('password');
+const alertAdm = document.getElementById('alert');
+
+const persona = {
+    user: 'admin1',
+    password: 'admin1',
+};
+
+formLoginAdm.onsubmit = function (e) {
+    e.preventDefault();
+    const user = inputUserAdm.value;
+    const password = inputPasswordAdm.value;
+    const usuarioValido = user === persona.user &&
+        password === persona.password;
+    if (usuarioValido) {
+        window.location.href = './index.html';
+        alert('Bienvenido/a');
+    } else {
+        alertAdm.classList.remove('d-none');
+        alert('Bienvenido/a');
+    }
+};
