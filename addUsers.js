f21f500952c7fa259e481d53124a6c0452fb504d
Eynieman <eynieman@gmail.com>
2020-11-26 10:01:09 -0300

    Todo hasta funcion suspender en admin

--- addUsers.js
+++ addUsers.js
@@ -1,10 +1,16 @@
+const userNameEdit = document.getElementById('userNameCreate');
+const nickNameEdit = document.getElementById('nicknameCreate');
+const emailEdit = document.getElementById('emailCreate');
+const birthDateEdit = document.getElementById('birthDateCreate');
+
+
 const loadForm = (userId) => {
     const users = JSON.parse(localStorage.getItem('users')) || [];
     const user = users.find((u) => u.id === userId);
-    userCreateInput.value = user.user;
-    nicknameCreateInput.value = user.nickname;
-    emailCreateInput.value = user.email;
-    birthDateCreateInput.value = user.birthDate;
+    userNameEdit.value = user.nombreApellido;
+    nickNameEdit.value = user.user;
+    emailEdit.value = user.email;
+    birthDateEdit.value = user.birthDateCreate;
     editUserId = userId;
 }
 
@@ -18,11 +24,11 @@ function displayUsers(users) {
             <td>${user.user}</td>
             <td>${user.birthDateCreate || ''}</td>
             <td>${user.email}</td>
-            <td><button type="button" class="btn btn-warning text-white" data-toggle="modal" data-target="#editModal" onclick="loadForm('${user.id}')"><i class="far fa-edit"></i></button>
+            <td><button type="button" class="btn btn-warning text-white" data-toggle="modal" data-target="#editUserModal" onclick="loadForm('${user.id}')"><i class="far fa-edit"></i></button>
             <td><button onclick="deleteUser('${user.id}')" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button></td>
             <td><button onclick="suspendUser('${user.id}')" class="btn btn-info"><i class="fab fa-expeditedssl"></i></button></td>
-        </tr>
-        `;
+            </tr>
+            `;
         rows.push(tr);
     }
     usersTable.innerHTML = rows.join('');
@@ -46,17 +52,17 @@ function deleteUser(userId) {
 formUserEdit.onsubmit = (e) => {
     e.preventDefault()
     const users = JSON.parse(localStorage.getItem('users')) || [];
-    const user = userCreateInput.value;
-    const nickname = nicknameCreateInput.value;
-    const email = emailCreateInput.value;
-    const birthDate = birthDateCreateInput.value;
+    const nombreApellido = userNameEdit.value;
+    const user = nickNameEdit.value;
+    const email = emailEdit.value;
+    const birthDate = birthDateEdit.value.value;
 
     const updatedUsers = users.map((u) => {
         if (u.id === editUserId) {
             const users = {
                 ...u,
+                nombreApellido,
                 user,
-                nickname,
                 email,
                 birthDate,
             }
@@ -68,7 +74,7 @@ formUserEdit.onsubmit = (e) => {
 
     const usersJson = JSON.stringify(updatedUsers);
     localStorage.setItem('users', usersJson);
-    formEdit.reset();
+    formUserEdit.reset();
     displayAllUsers();
     $('#editUserModal').modal('hide');
 }
