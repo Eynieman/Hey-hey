25d1733f4d5ef4ea20ee8bdacf8b89c66eb996a2
Eynieman <eynieman@gmail.com>
2020-12-05 14:05:07 -0300

    Identado htmls + delay carrusel

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /*---------------Función Swiper---------------*/
 var mySwiper = new Swiper('.swiper-container', {
-    speed: 1500,
+    speed: 2500,
     spaceBetween: 0,
     grabCursor: true,
     autoplay: {
         autoplay: true,
-        delay: 700, disableOnInteraction: false
+        delay: 1500, disableOnInteraction: false
     }
 });
 
